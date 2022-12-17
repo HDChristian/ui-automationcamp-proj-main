dfe3d174bbeaac8d08a83f61b810abe743da590f
HDchristian <hansdchristian@gmail.com>
2022-12-17 13:07:59 -0500

    changes

--- cypress-ui-autocamp-store/e2e/test/favorites/favorites.cy.js
+++ cypress-ui-autocamp-store/e2e/test/favorites/favorites.cy.js
@@ -23,12 +23,12 @@ const { default: favoritesPage } = require("../../page/favorites.page")
             Cart.addToCart('Quality Fitted Hat')
             Cart.navigateToCart()
     
-            // Assert that there is 1 item in the cart then remove the item
+            
             cy.get(Cart.cartQuantity).should('have.text', 1)
             cy.get(Cart.cartItemsName).should('have.text', 'Quality Fitted Hat')
-            cy.get(Cart.removeSauceLabBackPackBtn).click()
+            cy.get(Cart.removeQualityFittedHatBtn).click()
     
-            // Assert that the item was removed and the cart is empty
+            
             cy.get(Cart.cartItemsName).should('not.exist')
             cy.get(Cart.removedCartItem).should('exist')
         })
